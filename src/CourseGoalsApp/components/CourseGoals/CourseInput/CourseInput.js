933b696d0f065ff905eb63b027227da0f24874d4
Lex <alext@disroot.org>
2022-08-11 21:54:56 +0200

    Finish 6.79

--- src/CourseGoalsApp/components/CourseGoals/CourseInput/CourseInput.js
+++ src/CourseGoalsApp/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
 
 export default function CourseInput({ addGoalHandler }) {
   const [enteredValue, setEnteredValue] = useState('');
