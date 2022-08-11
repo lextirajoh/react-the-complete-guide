533d8df6b7d67172bb6fc16e92b486bf08c81149
Lex <alext@disroot.org>
2022-08-11 21:50:57 +0200

    Finish 6.78

--- src/CourseGoalsApp/components/CourseGoals/CourseInput/CourseInput.js
+++ src/CourseGoalsApp/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
 
@@ -26,11 +27,38 @@ export default function CourseInput({ addGoalHandler }) {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+      <FormControl invalid={!isValid}>
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </div>
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
 }
+
+const FormControl = styled.div`
+  margin: 0.5rem 0;
+
+ & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+    color: ${props => (props.invalid ? 'red' : 'black')}
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
+    background: ${props => (props.invalid ? 'red' : 'transparent')}
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+`;
