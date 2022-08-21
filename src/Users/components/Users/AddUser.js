f4ba6785fb67a25e1c0b5b44ffe990cb70f15279
Lex <alext@disroot.org>
2022-08-21 16:54:08 +0200

    Finish 8.91

--- src/Users/components/Users/AddUser.js
+++ src/Users/components/Users/AddUser.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Card from '../UI/Card';
+import classes from './AddUser.module.css';
 
 export default function AddUser() {
   function handleSubmit(e) {
@@ -7,13 +9,15 @@ export default function AddUser() {
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="username">Username</label>
-        <input id="username" type="text" placeholder="Enter your username" />
-        <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" placeholder="Enter your age" />
-        <button type="submit">Add User</button>
-      </form>
+      <Card className={classes.input} >
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="username">Username</label>
+          <input id="username" type="text" placeholder="Enter your username" />
+          <label htmlFor="age">Age (Years)</label>
+          <input id="age" type="number" placeholder="Enter your age" />
+          <button type="submit">Add User</button>
+        </form>
+      </Card>
     </>
   );
 }
