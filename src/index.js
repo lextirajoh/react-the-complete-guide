e2a187e220947c6e260803060eff5f8d15d4b3c2
Lex <alext@disroot.org>
2022-08-25 10:37:46 +0200

    Finish 20.268

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './ExpenseApp/App';
 // import App from './CourseGoalsApp/App'
 // import App from './CourseGoalsDebug/App'
-import App from './Users/App';
-import React from 'react';
+// import App from './Users/App';
+import App from './RouterApp/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
