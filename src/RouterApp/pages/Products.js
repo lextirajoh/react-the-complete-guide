0c805e462c0b1cde7cd61cb2e4dc7cd196b609a8
Lex <alext@disroot.org>
2022-08-25 10:52:31 +0200

    Finish 20.269

---
+++ src/RouterApp/pages/Products.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Products() {
+  return (
+    <div>
+      {' '}
+      <h1> The Products Page</h1>
+    </div>
+  );
+}
---
+++ src/RouterApp/pages/Welcome.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Welcome() {
+  return (
+    <div>
+      {' '}
+      <h1> The Welcome Page</h1>
+    </div>
+  );
+}
