ad39175f3ba45cf4bb0f4b8035f3c9e3df217df9
Lex <alext@disroot.org>
2022-08-25 15:05:00 +0200

    Finish 20.273

--- src/RouterApp/pages/ProductDetail.js
+++ src/RouterApp/pages/ProductDetail.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 export default function ProductDetail() {
+  const params = useParams();
+  console.log(params.productId)
+
   return (
     <>
       <section>
         <h1>Product Detail</h1>
+        <p>
+        {params.productId}
+        </p>
       </section>
     </>
   );
