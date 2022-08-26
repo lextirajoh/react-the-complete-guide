f8268399f868fa6abc124c6c0f415c19f7f939f4
Lex <alext@disroot.org>
2022-08-26 16:26:31 +0200

    Finish 18.234

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 // import App from './ExpenseApp/App';
 // import App from './CourseGoalsApp/App'
 // import App from './CourseGoalsDebug/App'
 // import App from './Users/App';
 // import App from './RouterApp/App';
 import App from './ReduxApp/App';
+import store from './ReduxApp/store/index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
       <App />
+    </Provider>
   </React.StrictMode>
 );
